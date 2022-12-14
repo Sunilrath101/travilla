f72b5a22580c5a438de8168ab4dbf4c6fd591e57
Sourav Kumar <kmrsourav786@gmail.com>
2022-12-14 21:19:14 +0530

    Day-2 code

---
+++ tripvillas/src/Components/HomeCard/HomeCard.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import style from './HomeCard.module.css'
+
+const HomeCard = () => {
+  return (
+    <div className={style.box}>
+      <h2>Fully Managed Communities By Tripvillas</h2>
+        <div>
+          <div>
+            <img src="https://d2vcelvjdj7n25.cloudfront.net/newhome/icons/housekeeper.svg" alt="" />
+            <p>High Quality housekeeping</p>
+          </div>
+          <div>
+            <img src="https://d2vcelvjdj7n25.cloudfront.net/newhome/icons/towel.svg" alt="" />
+            <p>Comfortable Linen and Toiletries</p>
+          </div>
+          <div>
+            <img src="https://d2vcelvjdj7n25.cloudfront.net/newhome/icons/bed.svg" alt="" />
+            <p>Quality Furniture & Fittings</p>
+          </div>
+          <div>
+            <img src="https://d2vcelvjdj7n25.cloudfront.net/newhome/icons/dinner.svg" alt="" />
+            <p>Food Delivery Or Central Restaurant</p>
+          </div>
+        </div>
+    </div>
+  )
+}
+
+export default HomeCard
