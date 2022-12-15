8fbd59055971f9639feb0af72fa812eee249874b
Sourav Kumar <kmrsourav786@gmail.com>
2022-12-15 23:50:29 +0530

    day-3 pushed

--- tripvillas/src/Components/Searchbar/Searchbar.jsx
+++ tripvillas/src/Components/Searchbar/Searchbar.jsx
@@ -1,21 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Button from '../Button/Button'
 import style from './Searchbar.module.css'
+import Calendar from '../Calendar/Calendars'
 
 const Searchbar = () => {
+
+  const [cal, setCal] = useState(false)
+
   return (
-    <form action="">
-        <input type="text" />
-        <select name="guest" id="">
-          <option selected value="">Select Guests</option>
-          <option value="">1 guest</option>
-          <option value="">2 guests</option>
-          <option value="">3 guests</option>
-          <option value="">4 guests</option>
-          <option value="">5 guests</option>
-        </select>
-        <Button text="SEARCH"/>
-    </form>
+    <div className={style.container}>
+      <form action="">
+        <div>
+          <input type="text" placeholder='Location'/>
+          <input type="text" placeholder='Check In' onClick={()=>setCal(!cal)}/>
+          <input type="text" placeholder='Check Out' />
+          <select name="guest" id="">
+            <option selected value="select-guests">Select Guests</option>
+            <option value="1-guest">1 guest</option>
+            <option value="2-guests">2 guests</option>
+            <option value="3-guests">3 guests</option>
+            <option value="4-guests">4 guests</option>
+            <option value="5-guests">5 guests</option>
+          </select>
+          <Button text="SEARCH"/>
+        </div>
+        {cal ? <Calendar/> : false}
+      </form>
+    </div>
   )
 }
 
