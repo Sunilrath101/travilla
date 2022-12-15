86993f93a31f98a708519cd769423fa3ccf09bde
Sunilrath101 <sunilrath.ssr@gmail.com>
2022-12-15 23:42:07 +0530

    input taking

--- tripvillas/src/Pages/AllRoutes.jsx
+++ tripvillas/src/Pages/AllRoutes.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import DashBoard from "../DashBoard/DashBoard";
+import CreateProperty from "./CreateProperty/CreateProperty";
 import Home from "./Home/Home";
 
 const AllRoutes = () => {
@@ -8,6 +9,7 @@ const AllRoutes = () => {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/dashBoard" element={<DashBoard />} />
+      <Route path="/host/create-property" element={<CreateProperty />} />
     </Routes>
   );
 };
